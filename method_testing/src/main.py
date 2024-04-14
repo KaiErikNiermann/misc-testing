eec533c6c69ed09e5d9ead88c657862a805f476c
KaiErikNiermann <kai.niermann@gmail.com>
2024-04-14 11:12:53 +0200

    updated code

--- method_testing/src/main.py
+++ method_testing/src/main.py
@@ -1,9 +1,9 @@
 class Testing: 
-    def give_first(self, nums): 
+    def give_first(self, nums: list[int]) -> int: 
         return nums[0]
     
-    def give_first_alt(self, nums): 
+    def give_first_alt(self, nums: list[int]) -> int: 
         return nums[:-1][0]
     
-print(Testing().give_first([1, 2, 3]))
-print(Testing().give_first_alt([1, 2, 3]))
+    def give_last(self, nums: list[int], num: int) -> int: 
+        return nums[-1]
--- method_testing/tests/tests.py
+++ method_testing/tests/tests.py
@@ -1,14 +1,50 @@
 from src import main
 
+import traceback
 import unittest
 import inspect
+import pprint
 from typing import Generator
-
 class TestSolution(unittest.TestCase): 
-    def method_gettr() -> Generator[callable, None, None]: 
+    @staticmethod
+    def method_gettr(func_filter) -> Generator[callable, None, None]: 
         for _, method in inspect.getmembers(
-            main.Testing, predicate=inspect.isfunction
+            main.Testing, predicate=func_filter
         ): yield method
+
+    def data_func(self, inout, problem, func_filter): 
+        for data, expected in inout:
+            for method in TestSolution.method_gettr(func_filter): 
+                print(inspect.signature(method))
+                self.assertEqual(method(problem.Testing(), data), expected)
+    
+    def test_give_first_sig_filt(self):
+        # create a signature of list[int]
+        t_signature = inspect.Signature(
+            parameters=[
+                inspect.Parameter(
+                    name="self", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD
+                ),
+                inspect.Parameter(
+                    name="nums", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=list[int]
+                )
+            ], return_annotation=int
+        )
+        
+        print(type(t_signature))
+        
+        self.data_func([
+            ([1, 2, 3], 1),
+            ([4, 5, 6], 4),
+            ([7, 8, 9], 7)
+        ], main, lambda x: inspect.isfunction(x) and inspect.signature(x) == t_signature)
+        
+    def test_give_first_name_filt(self):
+        self.data_func([
+            ([1, 2, 3], 1),
+            ([4, 5, 6], 4),
+            ([7, 8, 9], 7)
+        ], main, lambda x: inspect.isfunction(x) and x.__name__.startswith("give_first"))
         
     def test_give_first_best(self):
         inout = [
@@ -17,7 +53,7 @@ class TestSolution(unittest.TestCase):
             ([7, 8, 9], 7)
         ]
         for i in inout: 
-            for method in TestSolution.method_gettr(): 
+            for method in TestSolution.method_gettr(lambda x: inspect.isfunction(x) and x.__name__.startswith("give_first")): 
                 self.assertEqual(method(main.Testing(), i[0]), i[1])
         
     def test_give_first_bad(self): 
@@ -29,10 +65,11 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(main.Testing().give_first_alt([7, 8, 9]), 7)
         
     def test_give_first_better(self): 
-        for method in TestSolution.method_gettr(): 
+        for method in TestSolution.method_gettr(lambda x: inspect.isfunction(x) and x.__name__.startswith("give_first")): 
             self.assertEqual(method(main.Testing(), [1, 2, 3]), 1)
             self.assertEqual(method(main.Testing(), [4, 5, 6]), 4)
             self.assertEqual(method(main.Testing(), [7, 8, 9]), 7)
+
 
     def test_give_first_too_far(self):
         inout = [
@@ -40,7 +77,7 @@ class TestSolution(unittest.TestCase):
             ([4, 5, 6], 4),
             ([7, 8, 9], 7)
         ]
-        methods = [method for method in TestSolution.method_gettr()]
+        methods = [method for method in TestSolution.method_gettr(lambda x: inspect.isfunction(x) and x.__name__.startswith("give_first"))]
         for data, expected in inout: 
             results = map(lambda method: method(main.Testing(), data), methods)
             self.assertEqual(list(results), [expected] * len(methods))
